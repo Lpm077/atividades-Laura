59da0e7a895d288464bdf79d582d4bf16694e315
Lpm077 <kaiquegabriellopesdesouza@gmail.com>
2024-09-18 20:55:26 -0300

    Questão 2.c

--- AEDI-LISTA DE EXERCICIOS IIl-Kaique Gabriel Lopes de Souza-2º periodo/Questão 2.c
+++ AEDI-LISTA DE EXERCICIOS IIl-Kaique Gabriel Lopes de Souza-2º periodo/Questão 2.c
@@ -2,31 +2,34 @@
 
 int main() {
     int numeros[] = {5, 1, 4, 8, 3, 7};
-    int tamanho = 6; 
+    int tamanho = 6;
+
     int i, j, temp;
     int troca;
 
     for (i = 0; i < tamanho - 1; i++) {
-        troca = 0;  
+        troca = 0;
+
         for (j = 0; j < tamanho - i - 1; j++) {
-            if (numeros[j] > numeros[j + 1]) {  
+            if (numeros[j] > numeros[j + 1]) {
                 temp = numeros[j];
                 numeros[j] = numeros[j + 1];
                 numeros[j + 1] = temp;
                 troca++;
             }
         }
+
         if (troca == 0) {
-            break;  
+            break;
+        }
     }
 
     printf("Array em ordem crescente: ");
     for (i = 0; i < tamanho; i++) {
-        printf("%d ", numeros[i]);  
+        printf("%d ", numeros[i]);
     }
     printf("\n");
 
     return 0;
 }
-	}
 
